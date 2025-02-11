540ed054249712e5ebb6c3d9b4d375b3b5af19e7
GalahadDev <121528841+GalahadDev@users.noreply.github.com>
2025-02-11 17:58:37 -0300

    Add files via upload

--- API/models/Pedido.go
+++ API/models/Pedido.go
@@ -10,17 +10,17 @@ import (
 type Pedido struct {
 	ID            uint      `gorm:"primaryKey"`
 	Descripcion   string    `gorm:"size:255;not null"`
-	Imagen        string    `gorm:"size:255"`
+	Imagen        string    `gorm:"size:255;not null"`
 	FechaCreacion time.Time `gorm:"autoCreateTime"`
 	Fletero       *string   `gorm:"size:100;default:null"`
 	Monto         *float64  `gorm:"default:null"`
-	Estado        string    `gorm:"size:50;default:'No Entregado'"`
+	Estado        string    `gorm:"size:50;default:'Pendiente'"`
 	Precio        *float64  `gorm:"default:null"`
-	Nombre        string    `gorm:"size:55"`
-	Observaciones string    `gorm:"size:255"`
-	Forma_Pago    string    `gorm:"size:55"`
-	Direccion     string    `gorm:"size:255"`
-	Nro_Tlf       string    `gorm:"size:12"`
+	Nombre        string    `gorm:"size:55;not null"`
+	Observaciones string    `gorm:"size:255;not null"`
+	Forma_Pago    string    `gorm:"size:55;not null"`
+	Direccion     string    `gorm:"size:255;not null"`
+	Nro_Tlf       string    `gorm:"size:12;not null"`
 	Pagado        string    `gorm:"size:9;default:'No Pagado'"`
 	Atendido      bool      `gorm:"default:false"`
 
